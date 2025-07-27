a91e6bd3b4dedb86c2766bc0ec62be78c7a4534e
kuldeep-creates <kuldeepjangid2008@gmail.com>
2025-07-27 17:54:55 +0530

    update

--- src/App.js
+++ src/App.js
@@ -64,14 +64,6 @@ function App() {
       />
       <Alert alart={alert} />
       <div className="container">
-        <TextForm
-                heading="CONVERTOR"
-                darkMode={mode}
-                showAlert={showAlert}
-              />
-
-      </div>
-      <div className="container">
         <Routes>
           <Route
             path="/"
